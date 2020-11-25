c4ac24c5cdaced2d062fd9c5c06ce30a1f6dfc26
OlegKravets <o.kravets@evnedev.com>
2020-11-25 11:14:56 +0200

    Adding model observer for force-retrieving data from redis service

---
+++ src/Traits/UsesRedisService.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace OlegKravets\LaravelRedisService\Traits;
+
+
+use Exception;
+use OlegKravets\LaravelRedisService\Mutators\QueryBuilderMutator;
+use OlegKravets\LaravelRedisService\ServiceHandlers\OutboundStream;
+
+trait UsesRedisService
+{
+    /**
+     * This mechanism will prevent
+     *
+     * @return QueryBuilderMutator
+     * @throws Exception
+     */
+    protected function newBaseQueryBuilder(): QueryBuilderMutator
+    {
+        $conn = $this->getConnection();
+        $grammar = $conn->getQueryGrammar();
+        $builder = new QueryBuilderMutator($conn, $grammar, $conn->getPostProcessor());
+        if (!isset($this->_service_channel)) {
+            throw new Exception("Model does not contain service channel");
+        }
+
+        $builder->_service_channel = $this->_service_channel;
+        $builder->service_connection = new OutboundStream();
+        return $builder;
+    }
+}
