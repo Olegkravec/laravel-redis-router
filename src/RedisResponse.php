5ecfc3aff1157ecf87c89dec69b71f13f3e51537
wolkoffff <olegkravec97@gmail.com>
2020-12-09 12:25:36 +0300

    Update RedisResponse.php

--- src/RedisResponse.php
+++ src/RedisResponse.php
@@ -11,6 +11,9 @@ class RedisResponse
     public static function ok(string $message = "Handled.") : Collection {
         return new Collection(["status" => true, "message" => $message]);
     }
+    public static function success($data = [], $message = "Successfully handled"){
+        return new Collection(["status" => true, "message" => $message, "data" => $data]);
+    }
     public static function fail(string $message = "Handled.") : Collection {
         return new Collection(["status" => false, "message" => $message]);
     }
